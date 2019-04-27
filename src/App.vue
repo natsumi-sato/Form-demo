bac76eb7cbe30376fbc9211614f6ca0538ced1ae
NatsumiSato <natsumi.yamaguchi@noin.tv>
2019-04-27 22:24:00 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <img class="img" src="./assets/logo.png">
+      <p class="title">Cat LOVER <span>vue</span></p>
+    </div>
+    <div id="mount-point"></div>
+
+    <div class="content">
+      <router-view></router-view>
+    </div>
+    <div class="footer">
+      <h2>Cat LOVER Co., Ltd.</h2>
+      <ul>
+        <li><a href="" target="_blank">TOP</a></li>
+        <li><a href="" target="_blank">リクルート</a></li>
+        <li><a href="" target="_blank">アクセス</a></li>
+        <li><a href="" target="_blank">会社概要</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+
+</script>
+
+<style lang="scss">
+html {
+  height: 100%;
+}
+body{
+  height: 100%;
+  margin: 0;
+  background-color: silver;
+  text-align: center;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: block;
+  color: #2c3e50;
+  background-color: white;
+  width: 80%;
+  margin: 0 auto;
+  min-width: 600px;
+  margin: 0 auto;
+  height: 100%;
+}
+.header {
+  display: block;
+  .img {
+    display: inline-block;
+    width: 15%;
+    left: 20%;
+    display: inline-block;
+  }
+  .title{
+    display: inline-block;
+    width: 30%;
+    font-size:2rem;
+    span{
+      font-size: 2.5rem;
+      text-shadow: 1px 1px 3px #000;
+    }
+  }
+}
+.footer{
+  display: block;
+  position: absolute;
+  bottom: 0%;
+  width: 80%;
+  min-width: 600px;
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+}
+</style>
---
+++ src/components/Scene1.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="scene1">
+    <nickname-header></nickname-header>
+    <input-text></input-text>
+    <input-button></input-button>
+  </div>
+</template>
+
+<script>
+//  インポート
+import Vue from 'vue'
+import {NickNameHeader, InputText, InputButton} from './forms/Headers.js'
+
+//  デフォルト値定義
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+
+//  コンポーネントを登録
+Vue.component('nickname-header', NickNameHeader)
+//  コンポーネントを登録
+Vue.component('input-text', InputText)
+//  コンポーネントを登録
+Vue.component('input-button', InputButton)
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.scene1{
+  font-size: 1.8rem;
+  text-align: center;
+  a div p{
+    display: block;
+    margin: 0 auto;
+    height:30pt;
+  }
+}
+.nickname{
+  display: block;
+  height: 3rem;
+}
+.input{
+  width: 130pt;
+  height:30pt;
+}
+.button {
+  display: block;
+  position: relative;
+  margin: 0 auto;
+  width: 70pt;
+  background-color: white;
+  border: solid 1px silver;
+  border-radius: 0.5rem 0.5rem;
+  padding: 0.5rem 1.5rem;
+  margin-top: 1rem;
+  text-decoration: none;
+}
+
+</style>
